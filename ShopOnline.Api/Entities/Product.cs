3301f2e2e2e7dd360ae305d5fbbaa5202c5c116d
Nikola Vodeni4arov <nikolavodeni4arov@gmail.com>
2023-02-03 12:36:34 +0200

    Add project files.

---
+++ ShopOnline.Api/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopOnline.Api.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Column(TypeName="decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
